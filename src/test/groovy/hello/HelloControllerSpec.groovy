51bd5635530131874212b688644a8447c492bea4
Tomas Lin <tomaslin@gmail.com>
2013-10-24 03:12:49 +0100

    use autocleanup annotation

--- src/test/groovy/hello/HelloControllerSpec.groovy
+++ src/test/groovy/hello/HelloControllerSpec.groovy
@@ -1,23 +1,25 @@
 package hello
- 
+
 import org.springframework.boot.SpringApplication
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
+import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
- 
+
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit
- 
+
 class HelloControllerSpec extends Specification {
- 
+
     @Shared
+    @AutoCleanup
     ConfigurableApplicationContext context
- 
+
     void setupSpec() {
         Future future = Executors
                 .newSingleThreadExecutor().submit(
@@ -30,20 +32,14 @@ class HelloControllerSpec extends Specification {
                 })
         context = future.get(60, TimeUnit.SECONDS)
     }
- 
-    void cleanupSpec() {
-        if (context != null) {
-            context.close()
-        }
-    }
- 
+
     void "should return Greetings from Spring Boot!"() {
         when:
         ResponseEntity entity = new RestTemplate().getForEntity("http://localhost:8080", String.class)
- 
+
         then:
         entity.statusCode == HttpStatus.OK
         entity.body == 'Greetings from Spring Boot!'
     }
- 
+
 }
