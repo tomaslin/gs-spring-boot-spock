385fdc5891131591cb090cdb23d0fc9465d2a524
Tomas Lin <tomaslin@gmail.com>
2013-10-24 03:10:10 +0100

    add simple test file to see spock works

---
+++ src/test/groovy/hello/HelloControllerSpec.groovy
@@ -0,0 +1,12 @@
+package hello
+ 
+import spock.lang.Specification
+ 
+class HelloControllerSpec extends Specification{
+ 
+    void "testing spock works"(){
+        expect:
+        true
+    }
+ 
+}
