0bf347ce9d678c5df3f0a50fb839ab42d7645a23
Tomas Lin <tomaslin@gmail.com>
2013-10-24 03:18:58 +0100

    implement new logic

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,23 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
-    
+
+    @RequestMapping("/reverse/{stringToReverse}")
+    public
+    @ResponseBody
+    String reverse(@PathVariable String stringToReverse) {
+        return new StringBuilder(stringToReverse).reverse().toString();
+    }
+
 }
