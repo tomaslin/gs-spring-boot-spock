fcb1f630b3dfa215ab1699c0dfdde27ec2b1a84f
Tomas Lin <tomaslin@gmail.com>
2013-10-24 03:11:34 +0100

    write a full failing test

--- src/test/groovy/hello/HelloControllerSpec.groovy
+++ src/test/groovy/hello/HelloControllerSpec.groovy
@@ -1,12 +1,49 @@
 package hello
  
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.client.RestTemplate
+import spock.lang.Shared
 import spock.lang.Specification
  
-class HelloControllerSpec extends Specification{
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
  
-    void "testing spock works"(){
-        expect:
-        true
+class HelloControllerSpec extends Specification {
+ 
+    @Shared
+    ConfigurableApplicationContext context
+ 
+    void setupSpec() {
+        Future future = Executors
+                .newSingleThreadExecutor().submit(
+                new Callable() {
+                    @Override
+                    public ConfigurableApplicationContext call() throws Exception {
+                        return (ConfigurableApplicationContext) SpringApplication
+                                .run(Application.class)
+                    }
+                })
+        context = future.get(60, TimeUnit.SECONDS)
+    }
+ 
+    void cleanupSpec() {
+        if (context != null) {
+            context.close()
+        }
+    }
+ 
+    void "should return Greetings from Spring Boot!"() {
+        when:
+        ResponseEntity entity = new RestTemplate().getForEntity("http://localhost:8080", String.class)
+ 
+        then:
+        entity.statusCode == HttpStatus.OK
+        entity.body == 'No Greetings from Spring Boot!'
     }
  
 }
